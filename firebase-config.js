53de2c43f9bbcde6164304761fba15eb87f0ec53
cvmchocoisidoragestionhumana-alt <cvmchocoisidoragestionhumana@gmail.com>
2026-06-16 15:39:32 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,6 @@
-// ============================================================
-//  CONFIGURACION DE FIREBASE  -  personal-choco-cvm
-// ============================================================
+// =========================================================
+// CONFIGURACION DE FIREBASE  -  personal-choco-cvm
+// =========================================================
 const firebaseConfig = {
   apiKey: "AIzaSyCsi_mV-I8liJHdqcPVDXBMXjzOpXmFT3s",
   authDomain: "personal-choco-cvm.firebaseapp.com",
@@ -8,7 +8,8 @@ const firebaseConfig = {
   projectId: "personal-choco-cvm",
   storageBucket: "personal-choco-cvm.firebasestorage.app",
   messagingSenderId: "324713522900",
-  appId: "1:324713522900:web:0dcbb6e63114755be0bd41"
+  appId: "1:324713522900:web:0dcbb6e63114755be0bd41",
+  measurementId: "G-ZM75M87WJ6"
 };
 
 // No edites debajo de esta linea
